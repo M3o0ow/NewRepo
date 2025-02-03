2cd651c258954d0409d0c042da547e00f5a5f1d8
Michael Leblanc <emleblan64@monccnb.ca>
2025-02-03 10:06:19 -0400

    location

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,7 +37,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(197, 77);
+            this.button1.Location = new System.Drawing.Point(61, 81);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 145);
             this.button1.TabIndex = 0;
