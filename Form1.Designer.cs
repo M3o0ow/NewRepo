f408135eccce3c5832bba17908cfe37b2fda9c03
ruedman <samuelgl.chiasson@gmail.com>
2025-02-03 10:04:07 -0400

    oubi est rouge

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,12 +36,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.Location = new System.Drawing.Point(197, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(167, 145);
             this.button1.TabIndex = 0;
             this.button1.Text = "oubi";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
